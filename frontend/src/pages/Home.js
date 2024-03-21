43d53768a708ad7782ca5af1f18ccc30965cc824
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-21 16:01:02 +0530

    fetching data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,31 @@
-const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
 
-export default Home
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
